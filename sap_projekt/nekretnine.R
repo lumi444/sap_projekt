a00620c7dbb758cf41792bb03013a957e89605ef
lumi <lucijamicic154@gmail.com>
2022-12-19 21:27:30 +0100

    ovisnost broja soba i cijene kvadrata

--- sap_projekt/nekretnine.R
+++ sap_projekt/nekretnine.R
@@ -1,1 +1,14 @@
-#pocetak ...
+real_astate = read.csv("preprocessed_data.csv")
+real_astate$YearBuilt = as.integer(real_astate$YearBuilt)
+
+real_astate.grouped = aggregate(real_astate[c("GrLivArea","SalePrice")],
+                                real_astate[c("BedroomAbvGr")],
+                                FUN = sum)
+
+print(real_astate.grouped)
+
+barplot(real_astate.grouped$SalePrice/real_astate.grouped$GrLivArea,
+        main = "broj soba",
+        ylab = "cijena po kvadratu",
+        names.arg = real_astate.grouped$BedroomAbvGr,
+        las=2)
